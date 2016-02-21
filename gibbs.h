952850b5474c9cac807f6c1df25661cc98837758
Zhang Li <xiaoxin88214@163.com>
2016-02-21 22:38:40 +0800

    Add corpus.cc

---
+++ gibbs.h
@@ -0,0 +1,17 @@
+#ifndef GIBBS_H_
+#define GIBBS_H_
+
+#include <string>
+
+#include "corpus.h"
+#include "topic.h"
+#include "tree.h"
+#include "utils.h"
+
+namespace hlda {
+
+// The Gibbs state of the HLDA implemention.
+// Each Gibbs state has a corpus and a tree,
+// and keeps current scores, the current iteration and
+// the sampling parameters.
+}
