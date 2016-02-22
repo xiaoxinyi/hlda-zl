c207e5b148ab0aa4dd47adbede9c211bb5546bb4
Zhang Li <xiaoxin88214@163.com>
2016-02-22 22:37:33 +0800

    hlda_main.cc

---
+++ hlda_main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "gibbs.h"
+
+using hlda::GibbsSampler;
+using hlda::GibbsState;
+
+#define MAX_ITERATIONS 10000
+
+int main(int argc, char** argv) {
+	if (argc == 3) {
+		// The random number generator seed.
+		// For testing an example seed is: t = 1147530551;
+		log rng_seed;
+		(void)time(&rng_seed);
+
+		std::string filename_corpus = argv[1];
+		std::string filename_settings = argv[2];
+		hlda::GibbsState* gibbs_state = hlda::GibbsSampler::InitGibbsStateRep(
+				filename_corpus, filename_settings, rng_seed);
+
+		for (int i = 0; i < MAX_ITERATIONS; i++) {
+			hlda::GibbsSampler::IterateGibbsState(gibbs_state);
+		}
+
+		delete gibbs_state;
+	} else {
+		cout << "Arguments: "
+			"(1) corpus filename "
+			"(2) settings filename" << endl;
+	}
+	return 0;
+}
